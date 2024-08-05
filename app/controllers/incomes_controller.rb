2996abd238102962972d708fe5035359621bbc70
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-04 19:46:30 -0500

    income create action

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -4,4 +4,18 @@ class IncomesController < ApplicationController
 
     render :index 
   end
+
+  def create
+    @income = Income.new(income_params.merge(user_id: current_user.id))
+
+    if @income.save
+      render :show
+    else
+      render json: @income.errors, status: :unprocessable_entity
+    end
+  end
+
+  def income_params
+    params.permit(:amount, :source, :date)
+  end
 end
--- test/controllers/incomes_controller_test.rb
+++ test/controllers/incomes_controller_test.rb
@@ -1,11 +1,36 @@
 require "test_helper"
 
 class IncomesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(
+      name: "Jane",
+      email: "jane@test.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    post "/sessions.json", params: {email: "jane@test.com", password: "password"}
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/incomes.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal Income.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Income.count", 1 do
+      post "/incomes.json", params: {
+        amount: 12.50,
+        source: "work",
+        date: "2024-02-22",
+      }, headers: {
+        "Authorization" => "Bearer #{@jwt}"
+      }
+      assert_response 200
+    end
   end
 end
